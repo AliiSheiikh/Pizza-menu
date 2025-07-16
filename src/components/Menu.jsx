9d4414b5529777a60f1681608edbcbeda94864ed
Ali Sheikh <l_lawliet786@hotmail.com>
2025-07-15 20:21:26 -0400

    rendered pizzas by using map

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,23 +1,17 @@
 import Pizza from "./Pizza";
+import { pizzaData } from "/public/data.js";
 
 export default function Menu() {
   return (
     <>
       <main className="menu">
         <h2>Our menu</h2>
-        <Pizza
-          name="Pizza Spinaci"
-          ingredients="Tomato, mozarella, spinach, and ricotta cheese"
-          photoName="/pizzas/spinaci.jpg"
-          price="10"
-        />
 
-        <Pizza
-          name="Pizza Focaccia"
-          ingredients="Bread with italian olive oil and rosemary"
-          photoName="/pizzas/focaccia.jpg"
-          price="6"
-        />
+        <ul className="pizzas">
+          {pizzaData.map((pizza) => (
+            <Pizza pizzaObj={pizza} key={pizza.name} />
+          ))}
+        </ul>
       </main>
     </>
   );

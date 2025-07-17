25c2ba9f2903e51383ffe4378a3160368113d939
Ali Sheikh <l_lawliet786@hotmail.com>
2025-07-16 20:29:12 -0400

    added order button

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,12 @@
 export default function Footer() {
-
-    return(
-        <>
-        <footer className ="footer">{new Date().toLocaleTimeString()}. We're currently open!</footer>
-        </>
-    )
+  return (
+    <>
+      <footer className="footer">
+        <div className="order">
+          <p>We're open until 22:00.</p>
+          <button className="btn">Order</button>
+        </div>
+      </footer>
+    </>
+  );
 }
